3b3ee95f8e082206c0b6a9baa4a79a4c0ba4e4ee
morganherlocker <morgan.herlocker@gmail.com>
2014-10-30 12:16:26 -0400

    add support for geoms and features

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 // depend on jsts for now https://github.com/bjornharrtell/jsts/blob/master/examples/overlay.html
-var jsts = require('jsts')
-var featurecollection = require('turf-featurecollection')
+var jsts = require('jsts');
+var featurecollection = require('turf-featurecollection');
 
 module.exports = function(polys1, polys2){
-  var reader = new jsts.io.GeoJSONReader(),
-    a = reader.read(JSON.stringify(polys1.features[0].geometry)),
-    b = reader.read(JSON.stringify(polys2.features[0].geometry)),
-    intersection = a.intersection(b),
-    parser = new jsts.io.GeoJSONParser()
+  if(polys1.type === 'FeatureCollection') {
+  	polys1 = polys1.features[0];
+  } else if(polys1.type !== 'Feature') {
+  	polys1 = {
+  		type: 'Feature',
+  		geometry: polys1
+  	};
+  }
+  if(polys2.type === 'FeatureCollection') {
+  	polys2 = polys2.features[0];
+  } else if(polys2.type !== 'Feature') {
+  	polys2 = {
+  		type: 'Feature',
+  		geometry: polys2
+  	};
+  }
 
-  intersection = parser.write(intersection)
-  intersection = featurecollection([intersection])
+  var reader = new jsts.io.GeoJSONReader(),
+    a = reader.read(JSON.stringify(polys1.geometry)),
+    b = reader.read(JSON.stringify(polys2.geometry)),
+    intersection = a.intersection(b),
+    parser = new jsts.io.GeoJSONParser();
+
+  intersection = parser.write(intersection);
+  intersection = featurecollection([intersection]);
   return intersection;
 }
---
+++ test/test.js
@@ -0,0 +1,36 @@
+var intersect = require('../'),
+  test = require('tape'),
+  glob = require('glob'),
+  fs = require('fs');
+
+var REGEN = false;
+
+test('intersect', function(t){
+  glob.sync(__dirname + '/fixtures/in/*.json').forEach(function(input) {
+      var fcs = JSON.parse(fs.readFileSync(input));
+      var output = intersect(fcs[0], fcs[1]);
+      if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
+      t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
+  });
+  t.end();
+});
+
+test('intersect -- features', function(t){
+  glob.sync(__dirname + '/fixtures/in/*.json').forEach(function(input) {
+      var fcs = JSON.parse(fs.readFileSync(input));
+      var output = intersect(fcs[0].features[0], fcs[1].features[0]);
+      if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
+      t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
+  });
+  t.end();
+});
+
+test('intersect -- geometries', function(t){
+  glob.sync(__dirname + '/fixtures/in/*.json').forEach(function(input) {
+      var fcs = JSON.parse(fs.readFileSync(input));
+      var output = intersect(fcs[0].features[0].geometry, fcs[1].features[0].geometry);
+      if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
+      t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
+  });
+  t.end();
+});
