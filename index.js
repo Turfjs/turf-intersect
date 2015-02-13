120c25a5be54f38c0f8bfb9b6584f2314720558f
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-13 14:44:40 -0500

    Use geojson primitives in example

--- index.js
+++ index.js
@@ -11,26 +11,46 @@ var featurecollection = require('turf-featurecollection');
  * @param {Polygon} poly2 the second Polygon
  * @return {Polygon} a Polygon feature representing the area where `poly1` and `poly2` overlap
  * @example
- * var poly1 = turf.polygon([[
- *  [-122.801742, 45.48565],
- *  [-122.801742, 45.60491],
- *  [-122.584762, 45.60491],
- *  [-122.584762, 45.48565],
- *  [-122.801742, 45.48565]
- * ]]);
- * poly1.properties.fill = '#0f0';
- * var poly2 = turf.polygon([[
- *  [-122.520217, 45.535693],
- *  [-122.64038, 45.553967],
- *  [-122.720031, 45.526554],
- *  [-122.669906, 45.507309],
- *  [-122.723464, 45.446643],
- *  [-122.532577, 45.408574],
- *  [-122.487258, 45.477466],
- *  [-122.520217, 45.535693]
- * ]]);
- * poly2.properties.fill = '#00f';
- * var polygons = turf.featurecollection([poly1, poly2]);
+ * var poly1 = {
+ *   "type": "Feature",
+ *   "properties": {
+ *     "fill": "#0f0"
+ *   },
+ *   "geometry": {
+ *     "type": "Polygon",
+ *     "coordinates": [[
+ *       [-122.801742, 45.48565],
+ *       [-122.801742, 45.60491],
+ *       [-122.584762, 45.60491],
+ *       [-122.584762, 45.48565],
+ *       [-122.801742, 45.48565]
+ *     ]]
+ *   }
+ * }
+ * var poly2 = {
+ *   "type": "Feature",
+ *   "properties": {
+ *     "fill": "#00f"
+ *   },
+ *   "geometry": {
+ *     "type": "Polygon",
+ *     "coordinates": [[
+ *       [-122.520217, 45.535693],
+ *       [-122.64038, 45.553967],
+ *       [-122.720031, 45.526554],
+ *       [-122.669906, 45.507309],
+ *       [-122.723464, 45.446643],
+ *       [-122.532577, 45.408574],
+ *       [-122.487258, 45.477466],
+ *       [-122.520217, 45.535693]
+ *     ]]
+ *   }
+ * }
+ *
+ * var polygons = {
+ *   "type": "FeatureCollection",
+ *   "features": [poly1, poly2]
+ * };
  *
  * var intersection = turf.intersect(poly1, poly2);
  *
